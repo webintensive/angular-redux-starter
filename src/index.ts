f95ced408507beddf23b127d1ab18d1676359e0f
Seth Davenport <seth@rangle.io>
2016-03-13 19:12:55 -0400

    Enabled the Chrome extension version of redux dev tools.
    Cleaned up the way we configure the store to match the other starters more closely.
    
    Connected to rangle/rangle-starter#39

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
 import * as angular from 'angular';
-import rootReducer from './reducers';
 import { CounterComponent } from './components';
-
-const createLogger = require('redux-logger');
-const ngRedux = require('ng-redux');
-const thunk = require('redux-thunk').default;
+import './store/configure-store';
 
 // Global styles
 import './styles/index.css';
 
-declare var __DEV__: any;
-
-angular.module('counter', ['ngRedux'])
-  .config([
-    '$ngReduxProvider',
-    ($ngReduxProvider) => {
-      const logger = createLogger();
-      let middleware = [thunk];
-
-      if (__DEV__) {
-        middleware.push(logger);
-      }
-
-      $ngReduxProvider.createStoreWith(rootReducer, middleware);
-    }
-  ])
+angular.module('counter', [ 'counter.store' ])
   .directive('ngrCounter', CounterComponent);
 
 angular.element(document).ready(
-  () => angular.bootstrap(document, ['counter']));
+  () => angular.bootstrap(document, [ 'counter' ]));
